a62f0af23d8d178d5c915011dba8273794fa09bc
jspauldo98 <jaredspaulding1998@gmail.com>
2021-02-02 11:55:13 -0700

    Added Portfolio Model && Updated DB changelog

---
+++ smart_stock/smart_stock/Models/Portfolio.cs
@@ -0,0 +1,26 @@
+using System.Net.Mime;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace smart_stock.Models
+{
+    public class Portfolio
+    {
+        [Key]
+        [Column(TypeName = "int(64)")]
+        public int? Id { get; set; }
+
+        [Required]
+        public User User { get; set; }
+
+        [Column(TypeName = "double")]
+        public double Profit { get; set; }
+
+        [Column(TypeName = "double")]
+        public double Loss { get; set; }
+
+        [Column(TypeName = "double")]
+        public double Net { get; set; }
+    }
+}
