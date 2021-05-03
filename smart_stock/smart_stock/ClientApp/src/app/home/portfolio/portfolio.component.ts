ccc9edfa3f284eabadfdfba65337dc8b2a2c58a1
Jared Spaulding <jaredspaulding1998@gmail.com>
2021-05-03 07:45:37 -0600

    Chart (#31)
    * begin setup for db reading
    
    * Charting for specific Trade Account
    
    * Charts for trade account lists
    
    * Portfolio Chart & IMPORTANT DB change
    
    * removed point markers
    
    Co-authored-by: Robert Michaud <rmichau1@uwyo.edu>

--- smart_stock/smart_stock/ClientApp/src/app/home/portfolio/portfolio.component.ts
+++ smart_stock/smart_stock/ClientApp/src/app/home/portfolio/portfolio.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { IPortfolio, ITradeAccount, IUser, ICredential } from '../../interfaces';
+import { IPortfolio, ITradeAccount, IUser, ICredential, ILog } from '../../interfaces';
 import { LoginService } from '../../services/login.service';
 import { UserService } from '../../services/user.service';
 import { PortfolioService } from '../../services/portfolio.service';
+import { HistoryService } from 'src/app/services/history.service';
 import { first } from 'rxjs/operators';
 import { SubSink } from 'subsink';
+import { Chart } from 'angular-highcharts';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio',
@@ -19,38 +22,26 @@ export class PortfolioComponent implements OnInit, OnDestroy {
   public tradeAccount : ITradeAccount;
   public user: IUser;
   public content : number;
-   title = '\'s Total History';
-   type = 'LineChart';
-   data = [
-      ["Jan",  7.0],
-      ["Feb",  6.9],
-      ["Mar",  9.5],
-      ["Apr",  14.5],
-      ["May",  18.2],
-      ["Jun",  21.5],
-      ["Jul",  25.2],
-      ["Aug",  26.5],
-      ["Sep",  23.3],
-      ["Oct",  18.3],
-      ["Nov",  13.9],
-      ["Dec",  9.6]
-   ];
-   columnNames = ["Month", "Tokyo"];
-   options = {   
-      hAxis: {
-         title: 'Month'
-      },
-      vAxis:{
-         title: 'Temperature'
-      },
-   };
-  width = 1150;
-  height = 300;
-
+  public log : ILog[];
+  public chart : any;
+  public minuteHeaders = new Array();
+  public minuteData = new Array();
+  public hourHeaders = new Array();
+  public hourData = new Array();
+  public dayHeaders = new Array();
+  public dayData = new Array();
+  public weekHeaders = new Array();
+  public weekData = new Array();
+  public monthHeaders = new Array();
+  public monthData = new Array();
+  public yearHeaders = new Array();
+  public yearData = new Array();
 
   constructor(private readonly loginService : LoginService, 
     private readonly portfolioService : PortfolioService,
-    private readonly userService: UserService) { }
+    private readonly userService: UserService,
+    private readonly historyService : HistoryService,
+    private datepipe : DatePipe) { }
 
   ngOnInit(): void {
     this.getData();
@@ -67,9 +58,316 @@ export class PortfolioComponent implements OnInit, OnDestroy {
       }),
       this.portfolioService.getPortfolio(this.userCredentials).subscribe(res => {
         this.portfolio = res;
-        console.log(res);
+        this.historyService.getMinuteDataByPortfolio(res.id).subscribe(res => {
+          res.forEach(e => {
+            this.minuteHeaders.push(this.datepipe.transform(new Date(e.date), 'HH:mm'));
+            this.minuteData.push(e.portfolioAmount);          
+          });
+          this.minute();
+        }),
+        this.historyService.getHourDataByPortfolio(res.id).subscribe(res => {
+          res.forEach(e => {
+            this.hourHeaders.push(this.datepipe.transform(new Date(e.date), 'M-d-yy, HH:mm'));
+            this.hourData.push(e.portfolioAmount);
+          })
+        }),
+        this.historyService.getDayDataByPortfolio(res.id).subscribe(res => {
+          res.forEach(e => {
+            this.dayHeaders.push(this.datepipe.transform(new Date(e.date), 'MM-dd'));
+            this.dayData.push(e.portfolioAmount);
+          })
+        }),
+        this.historyService.getWeekDataByPortfolio(res.id).subscribe(res => {
+          res.forEach(e => {
+            this.weekHeaders.push(this.datepipe.transform(new Date(e.date), 'yyyy-ww'));
+            this.weekData.push(e.portfolioAmount);
+          })
+        }),
+        this.historyService.getMonthDataByPortfolio(res.id).subscribe(res => {
+          res.forEach(e => {
+            this.monthHeaders.push(this.datepipe.transform(new Date(e.date), 'MMM'));
+            this.monthData.push(e.portfolioAmount);
+          })
+        }),
+        this.historyService.getYearData(res.id).subscribe(res => {
+          res.forEach(e => {
+            this.yearHeaders.push(this.datepipe.transform(new Date(e.date), 'yyyy'));
+            this.yearData.push(e.portfolioAmount);
+          })
+        })
       })
     );
+  }
+
+  minute() {
+    this.chart = new Chart({
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: 'Equity v. Date/Time'
+      },
+      subtitle : {
+        text : 'Minutely'
+      },
+      credits: {
+        enabled: false
+      },
+      legend : {
+        enabled : false
+      },
+      yAxis : {
+        title : {
+          text : 'Equity'
+        }
+      },
+      xAxis : {
+        type : 'datetime',
+        title : {
+          text : 'Date/Time'
+        },
+        categories : this.minuteHeaders
+      },
+      plotOptions : {
+        line : {
+          marker : {
+            enabled : false
+          }
+        }
+      },
+      series: [
+        {
+          name: 'Equity',
+          type: 'line',
+          data: this.minuteData
+        },
+      ]
+    });
+  }
+  hour() {
+    this.chart = new Chart({
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: 'Equity v. Date/Time'
+      },
+      subtitle : {
+        text : 'Hourly'
+      },
+      credits: {
+        enabled: false
+      },
+      legend : {
+        enabled : false
+      },
+      yAxis : {
+        title : {
+          text : 'Equity'
+        }
+      },
+      xAxis : {
+        type : 'datetime',
+        title : {
+          text : 'Date/Time'
+        },
+        categories : this.hourHeaders
+      },
+      plotOptions : {
+        line : {
+          marker : {
+            enabled : false
+          }
+        }
+      },
+      series: [
+        {
+          name: 'Equity',
+          type: 'line',
+          data: this.hourData
+        },
+      ]
+    });
+  }
+  day() {
+    this.chart = new Chart({
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: 'Equity v. Date/Time'
+      },
+      subtitle : {
+        text : 'Daily'
+      },
+      credits: {
+        enabled: false
+      },
+      legend : {
+        enabled : false
+      },
+      yAxis : {
+        title : {
+          text : 'Equity'
+        }
+      },
+      xAxis : {
+        type : 'datetime',
+        title : {
+          text : 'Date/Time'
+        },
+        categories : this.dayHeaders
+      },
+      plotOptions : {
+        line : {
+          marker : {
+            enabled : false
+          }
+        }
+      },
+      series: [
+        {
+          name: 'Equity',
+          type: 'line',
+          data: this.dayData
+        },
+      ]
+    });
+  }
+  week() {
+    this.chart = new Chart({
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: 'Equity v. Date/Time'
+      },
+      subtitle : {
+        text : 'Weekly'
+      },
+      credits: {
+        enabled: false
+      },
+      legend : {
+        enabled : false
+      },
+      yAxis : {
+        title : {
+          text : 'Equity'
+        }
+      },
+      xAxis : {
+        type : 'datetime',
+        title : {
+          text : 'Date/Time'
+        },
+        categories : this.weekHeaders
+      },
+      plotOptions : {
+        line : {
+          marker : {
+            enabled : false
+          }
+        }
+      },
+      series: [
+        {
+          name: 'Equity',
+          type: 'line',
+          data: this.weekData
+        },
+      ]
+    });
+  }
+  month() {
+    this.chart = new Chart({
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: 'Equity v. Date/Time'
+      },
+      subtitle : {
+        text : 'Monthly'
+      },
+      credits: {
+        enabled: false
+      },
+      legend : {
+        enabled : false
+      },
+      yAxis : {
+        title : {
+          text : 'Equity'
+        }
+      },
+      xAxis : {
+        type : 'datetime',
+        title : {
+          text : 'Date/Time'
+        },
+        categories : this.monthHeaders
+      },
+      plotOptions : {
+        line : {
+          marker : {
+            enabled : false
+          }
+        }
+      },
+      series: [
+        {
+          name: 'Equity',
+          type: 'line',
+          data: this.monthData
+        },
+      ]
+    });
+  }
+  year() {
+    this.chart = new Chart({
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: 'Equity v. Date/Time'
+      },
+      subtitle : {
+        text : 'Yearly'
+      },
+      credits: {
+        enabled: false
+      },
+      yAxis : {
+        title : {
+          text : 'Equity'
+        }
+      },
+      legend : {
+        enabled : false
+      },
+      xAxis : {
+        type : 'datetime',
+        title : {
+          text : 'Date/Time'
+        },
+        categories : this.yearHeaders
+      },
+      plotOptions : {
+        line : {
+          marker : {
+            enabled : false
+          }
+        }
+      },
+      series: [
+        {
+          name: 'Equity',
+          type: 'line',
+          data: this.yearData
+        },
+      ]
+    });
   }
 
   ngOnDestroy() {
--- smart_stock/smart_stock/Services/ILogProvider.cs
+++ smart_stock/smart_stock/Services/ILogProvider.cs
@@ -9,5 +9,27 @@ namespace smart_stock.Services
         Task<bool> RecordTradeInLog(Log log);
         
         Task<IEnumerable<Log>> GetLog(int tId);
+
+        Task<IEnumerable<Log>> GetMinuteLog(int tId);
+
+        Task<IEnumerable<Log>> GetHourLog(int tId);
+
+        Task<IEnumerable<Log>> GetDayLog(int tId);
+        Task<IEnumerable<Log>> GetWeekLog(int tId);
+
+        Task<IEnumerable<Log>> GetMonthLog(int tId);
+
+        Task<IEnumerable<Log>> GetYearLog(int tId);
+
+        Task<IEnumerable<Log>> GetMinuteLogByPortfolio(int pId);
+
+        Task<IEnumerable<Log>> GetHourLogByPortfolio(int pId);
+
+        Task<IEnumerable<Log>> GetDayLogByPortfolio(int pId);
+        Task<IEnumerable<Log>> GetWeekLogByPortfolio(int pId);
+
+        Task<IEnumerable<Log>> GetMonthLogByPortfolio(int pId);
+
+        Task<IEnumerable<Log>> GetYearLogByPortfolio(int pId);
     }
 }
